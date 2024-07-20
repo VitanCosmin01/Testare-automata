c48c9257371310ec521a0f4e988c3eba04fe9aa8
VitanCosmin01 <137814408+VitanCosmin01@users.noreply.github.com>
2024-07-20 11:34:21 +0200

    ...

---
+++ Materiale Noi - TA, in urma PY/Saptamana 9/recap_notiuni_python.py
@@ -0,0 +1,62 @@
+"""
+1. Ce este o variabila in Python?
+"""
+
+"""
+2. Cum afisam un mesaj in consola?
+"""
+
+"""
+3. Ce tipuri de date cunosti?
+"""
+
+"""
+4. Cum putem extrage un subsir de caractere dintr-un string?
+"""
+
+"""
+5. Caracterieaza urmatoarele structuri de date din punct de vedere
+al mutabilitatii (mutabil/imutabil) si al ordonarii (ordonat/neordonat):
+- liste
+- dictionare
+- tupluri
+- seturi
+"""
+
+"""
+6. Ce este o exceptie in Python si cum o putem trata?
+"""
+
+"""
+7. Ce sunt functiile in Python?
+"""
+
+"""
+8. Ce sunt clasele in Python?
+"""
+
+"""
+9. Care sunt principiile OOP?
+"""
+
+
+"""
+10. Ce face blocul else atasat unui ciclu repetitiv?
+"""
+
+"""
+11. Cum putem accesa o valoare dintr-un dictionar?
+"""
+
+"""
+12. Cum putem inversa un string?
+"""
+
+
+"""
+13. Care este diferenta dintre o interfata si o clasa abstracta?
+"""
+
+"""
+14. Ce reprezinta parametru self folosit in clase?
+"""
